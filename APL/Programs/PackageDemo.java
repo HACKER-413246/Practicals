f84a8cd7ddea90e4d0f246d24e4559d45ec379a9
AVI <168284446+HACKER-413246@users.noreply.github.com>
2025-01-11 13:36:26 +0530

    Delete APL directory

--- APL/Programs/PackageDemo.java
+++
@@ -1,8 +0,0 @@
-import mypack.Box;  // Importing the Box class from mypack package
-
-public class PackageDemo {
-    public static void main(String[] args) {
-        Box b1 = new Box(); // Create an instance of Box
-        b1.display();       // Call the display method
-    }
-}
